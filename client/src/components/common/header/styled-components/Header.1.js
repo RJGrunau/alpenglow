e71923db7c7a1895e3fa15a9e7604cc1d9c796d8
rjgrunau <robertgrunau@gmail.com>
2019-12-10 21:29:42 -0800

    refactoring

---
+++ client/src/components/common/header/styled-components/Header.1.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const Header = styled.header`
+  width: 100%;
+  padding: 5px;
+  border-bottom: 3px solid red;
+  height: 150px;
+`;
---
+++ client/src/components/common/header/styled-components/HeaderContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const HeaderContainer = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr 1fr;
+`;
